8b900b3c39f328a9785bf509f3fb49202c957e87
Collin OConnell <ococncol@gmail.com>
2017-06-27 09:42:10 -0500

    Created PetContract Class and made the constants for the table and the gender values

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,29 @@
+package com.example.android.pets.data;
+
+
+import android.provider.BaseColumns;
+
+public final class PetContract {
+
+    private PetContract() {}
+
+    public static abstract class PetEntry implements BaseColumns {
+
+        /* CONSTANTS FOR TABLE INFO */
+        // Table name
+        public static final String TABLE_NAME = "pets";
+
+        // Column headings
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+        /* CONSTANTS FOR GENDER */
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+        public static final int GENDER_UNKNOWN = 0;
+    }
+
+}
